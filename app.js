e0f1228652ee2078039002b8ac9fc391ca3a2978
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-07 19:43:37 +0400

    added app.js logic

---
+++ app.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import path from 'path';
+import morgan from 'morgan';
+import express from 'express';
+import {createServer} from 'http';
+
+import routes from './routes/index.js';
+import errorHandler from '.milddlewares/errorHandler.js';
+
+const app = express();
+
+const {PORT} = process.env || 3000;
+
+app.set('views', path.resolve('views'));
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve('public')));
+
+app.use(routes);
+
+app.use(errorHandler.notFound);
+app.use(errorHandler.errors);
+
+const server = createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
